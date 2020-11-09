40a0af72d93016db8ed000777924180255f6713c
Liu-Jiannan <207726865@qq.com>
2020-11-09 20:11:52 +0800

    初识goroutine

---
+++ 2020.11.9_goroutine/1、goroutine.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	/*
+	一个goroutine打印数字，另一个goroutine打印数字，观察运行结果。。
+	 */
+
+	//1.先创建并启动子goroutine，执行printNum()
+	go printNum()
+	//2.main中打印字母
+	for i:=1;i<=100;i++  {
+		fmt.Printf("\t主goroutine中打印字母:A%d\n",i)
+	}
+
+	time.Sleep(1*time.Second)
+	fmt.Println("main...over")
+}
+
+func printNum()  {
+	for i := 1; i <= 100;i++{
+		fmt.Printf("子goroutine中打印数字:%d\n",i)
+	}
+}
